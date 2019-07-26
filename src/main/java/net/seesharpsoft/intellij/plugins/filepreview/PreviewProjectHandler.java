b37c54fa508ab5a00a5368177fdcffd0e4846d13
Sommer <m.sommer@sap.com>
2019-07-26 10:46:06 +0200

    [INTERNAL] first release

---
+++ src/main/java/net/seesharpsoft/intellij/plugins/filepreview/PreviewProjectHandler.java
@@ -0,0 +1,115 @@
+package net.seesharpsoft.intellij.plugins.filepreview;
+
+import com.intellij.ide.projectView.ProjectView;
+import com.intellij.ide.projectView.ProjectViewNode;
+import com.intellij.ide.projectView.impl.AbstractProjectViewPane;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.fileEditor.*;
+import com.intellij.openapi.fileEditor.impl.FileEditorManagerImpl;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Pair;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.util.messages.MessageBusConnection;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
+import javax.swing.tree.DefaultMutableTreeNode;
+
+public class PreviewProjectHandler {
+
+    private Project myProject;
+    private PreviewVirtualFile myPreviewFile;
+
+    private final TreeSelectionListener myTreeSelectionListener = new TreeSelectionListener() {
+        @Override
+        public void valueChanged(TreeSelectionEvent treeSelectionEvent) {
+            closePreview();
+
+            Object userObject = ((DefaultMutableTreeNode)treeSelectionEvent.getPath().getLastPathComponent()).getUserObject();
+            if (!(userObject instanceof ProjectViewNode)) {
+                return;
+            }
+            ProjectViewNode projectViewNode = (ProjectViewNode)userObject;
+            VirtualFile currentFile = projectViewNode.getVirtualFile();
+            if (currentFile == null || currentFile.isDirectory() || !currentFile.isValid()) {
+                return;
+            }
+
+            FileEditorManager fileEditorManager = FileEditorManager.getInstance(myProject);
+            if (!fileEditorManager.isFileOpen(currentFile)) {
+                currentFile = createAndSetPreviewFile(currentFile);
+            }
+            ApplicationManager.getApplication().assertIsDispatchThread();
+            fileEditorManager.openFile(currentFile, false);
+        }
+    };
+
+    private final FileEditorManagerListener myFileEditorManagerListener = new FileEditorManagerListener() {
+        @Override
+        public void fileOpenedSync(@NotNull FileEditorManager source, @NotNull VirtualFile file,
+                                   @NotNull Pair<FileEditor[], FileEditorProvider[]> editors) {
+            if (!(file instanceof PreviewVirtualFile)) {
+                closePreview();
+            }
+        }
+
+        @Override
+        public void selectionChanged(@NotNull FileEditorManagerEvent event) {
+            if (!(event.getNewProvider() instanceof PreviewEditorProvider)) {
+                closePreview();
+            }
+        }
+    };
+
+    public static final PreviewProjectHandler createIfPossible(@NotNull Project project, @NotNull MessageBusConnection messageBusConnection) {
+        PreviewProjectHandler previewProjectHandler = new PreviewProjectHandler();
+        if (previewProjectHandler.init(project, messageBusConnection)) {
+            return previewProjectHandler;
+        }
+        return null;
+    }
+
+    public boolean init(@NotNull Project project, @NotNull MessageBusConnection messageBusConnection) {
+        assert myProject == null : "already initialized";
+
+        myProject = project;
+
+        AbstractProjectViewPane viewPane = ProjectView.getInstance(myProject).getCurrentProjectViewPane();
+        if (viewPane == null) {
+            return false;
+        }
+
+        viewPane.getTree().addTreeSelectionListener(myTreeSelectionListener);
+        messageBusConnection.subscribe(FileEditorManagerListener.FILE_EDITOR_MANAGER, myFileEditorManagerListener);
+        return true;
+    }
+
+    public void dispose() {
+        assert myProject != null : "not initialized yet";
+
+        closePreview();
+
+        AbstractProjectViewPane viewPane = ProjectView.getInstance(myProject).getCurrentProjectViewPane();
+        if (viewPane != null) {
+            viewPane.getTree().removeTreeSelectionListener(myTreeSelectionListener);
+        }
+
+        myProject = null;
+    }
+
+    public void closePreview() {
+        if (myPreviewFile != null) {
+            FileEditorManagerImpl fileEditorManager = (FileEditorManagerImpl)FileEditorManager.getInstance(myProject);
+            fileEditorManager.closeFile(myPreviewFile, false, true);
+            myPreviewFile = null;
+        }
+    }
+
+    public VirtualFile createAndSetPreviewFile(VirtualFile file) {
+        assert myPreviewFile == null : "only one preview file at a time";
+
+        myPreviewFile = new PreviewVirtualFile(file);
+        return myPreviewFile;
+    }
+}
--- src/main/java/net/seesharpsoft/intellij/plugins/filepreview/PreviewStartupActivity.java
+++ src/main/java/net/seesharpsoft/intellij/plugins/filepreview/PreviewStartupActivity.java
@@ -1,147 +1,72 @@
 package net.seesharpsoft.intellij.plugins.filepreview;
 
-import com.intellij.ide.projectView.ProjectView;
-import com.intellij.ide.projectView.ProjectViewNode;
-import com.intellij.ide.projectView.impl.AbstractProjectViewPane;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.fileEditor.*;
-import com.intellij.openapi.fileEditor.impl.EditorTabbedContainer;
-import com.intellij.openapi.fileEditor.impl.FileEditorManagerImpl;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManager;
 import com.intellij.openapi.project.ProjectManagerListener;
 import com.intellij.openapi.startup.StartupActivity;
-import com.intellij.openapi.util.Pair;
-import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowId;
 import com.intellij.openapi.wm.ex.ToolWindowManagerListener;
-import com.intellij.util.messages.MessageBus;
 import com.intellij.util.messages.MessageBusConnection;
 import org.jetbrains.annotations.NotNull;
 
-import javax.swing.event.TreeSelectionEvent;
-import javax.swing.event.TreeSelectionListener;
-import javax.swing.tree.DefaultMutableTreeNode;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PreviewStartupActivity implements StartupActivity {
 
-    private Project myProject;
-    private PreviewVirtualFile myPreviewFile;
-    private boolean initialized = false;
+    Map<Project, PreviewProjectHandler> myPreviewHandlerMap = new HashMap<>();
 
-    private final TreeSelectionListener myTreeSelectionListener = new TreeSelectionListener() {
-        @Override
-        public void valueChanged(TreeSelectionEvent treeSelectionEvent) {
-            closePreview();
-
-            Object userObject = ((DefaultMutableTreeNode)treeSelectionEvent.getPath().getLastPathComponent()).getUserObject();
-            if (!(userObject instanceof ProjectViewNode)) {
-                return;
-            }
-            ProjectViewNode projectViewNode = (ProjectViewNode)userObject;
-            VirtualFile currentFile = projectViewNode.getVirtualFile();
-            if (currentFile == null || currentFile.isDirectory() || !currentFile.isValid()) {
-                return;
-            }
-
-            FileEditorManager fileEditorManager = FileEditorManager.getInstance(myProject);
-            if (!fileEditorManager.isFileOpen(currentFile)) {
-                currentFile = getPreviewFile(currentFile);
-            }
-            ApplicationManager.getApplication().assertIsDispatchThread();
-            fileEditorManager.openFile(currentFile, false);
-        }
-    };
-
-    public void startup() {
-        if (initialized) {
+    protected void register(Project project, MessageBusConnection connection) {
+        if (myPreviewHandlerMap.containsKey(project)) {
             return;
         }
-
-        AbstractProjectViewPane viewPane = ProjectView.getInstance(myProject).getCurrentProjectViewPane();
-        assert viewPane != null;
-
-        viewPane.getTree().addTreeSelectionListener(myTreeSelectionListener);
-        initialized = true;
-    }
-
-    public void shutdown() {
-        closePreview();
-
-        AbstractProjectViewPane viewPane = ProjectView.getInstance(myProject).getCurrentProjectViewPane();
-        assert viewPane != null;
-
-        viewPane.getTree().removeTreeSelectionListener(myTreeSelectionListener);
-
-        initialized = false;
-    }
-
-    public void closePreview() {
-        if (myPreviewFile != null) {
-            FileEditorManagerImpl fileEditorManager = (FileEditorManagerImpl)FileEditorManager.getInstance(myProject);
-            ApplicationManager.getApplication().assertIsDispatchThread();
-            fileEditorManager.closeFile(myPreviewFile, false, true);
-            myPreviewFile = null;
+        PreviewProjectHandler projectHandler = PreviewProjectHandler.createIfPossible(project, connection);
+        if (projectHandler != null) {
+            myPreviewHandlerMap.put(project, projectHandler);
         }
     }
 
-    public VirtualFile getPreviewFile(VirtualFile file) {
-        assert myPreviewFile == null;
-
-        myPreviewFile = new PreviewVirtualFile(file);
-        return myPreviewFile;
+    protected void unregister(Project project, MessageBusConnection connection) {
+        PreviewProjectHandler projectHandler = myPreviewHandlerMap.get(project);
+        if (projectHandler != null) {
+            myPreviewHandlerMap.remove(project);
+            projectHandler.dispose();
+            connection.disconnect();
+            connection.dispose();
+        }
     }
 
     @Override
-    public void runActivity(@NotNull Project project) {
-        myProject = project;
+    public void runActivity(@NotNull Project activityProject) {
+        if (myPreviewHandlerMap.containsKey(activityProject)) {
+            return;
+        }
 
-        startup();
-
-        MessageBus messageBus = ApplicationManager.getApplication().getMessageBus();
-        MessageBusConnection connection = messageBus.connect();
-
+        MessageBusConnection connection = activityProject.getMessageBus().connect();
         connection.subscribe(ToolWindowManagerListener.TOPIC, new ToolWindowManagerListener() {
             @Override
             public void toolWindowRegistered(@NotNull String id) {
                 if (ToolWindowId.PROJECT_VIEW.equals(id)) {
-                    startup();
+                    register(activityProject, connection);
                 }
             }
-
             @Override
             public void toolWindowUnregistered(@NotNull String id, @NotNull ToolWindow toolWindow) {
                 if (ToolWindowId.PROJECT_VIEW.equals(id)) {
-                    shutdown();
+                    unregister(activityProject, connection);
                 }
             }
         });
-
         connection.subscribe(ProjectManager.TOPIC, new ProjectManagerListener() {
             @Override
             public void projectClosingBeforeSave(@NotNull Project project) {
-                if (myProject.equals(project)) {
-                    shutdown();
+                if (activityProject.equals(project)) {
+                    unregister(activityProject, connection);
                 }
             }
         });
-
-        connection.subscribe(FileEditorManagerListener.FILE_EDITOR_MANAGER, new FileEditorManagerListener() {
-            @Override
-            public void fileOpenedSync(@NotNull FileEditorManager source, @NotNull VirtualFile file,
-                                       @NotNull Pair<FileEditor[], FileEditorProvider[]> editors) {
-                if (!(file instanceof PreviewVirtualFile)) {
-                    closePreview();
-                }
-            }
-
-            @Override
-            public void selectionChanged(@NotNull FileEditorManagerEvent event) {
-                if (!(event.getNewProvider() instanceof PreviewEditorProvider)) {
-                    closePreview();
-                }
-            }
-        });
+        // try to register at startup
+        register(activityProject, connection);
     }
 }
